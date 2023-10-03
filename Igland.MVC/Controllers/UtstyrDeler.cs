821a929e754846290883a1c1da732f460e1a6186
SiMENhol <simenhol@uia.no>
2023-10-03 19:40:52 +0200

    La til flere kontrollere, samt nav bar

---
+++ Igland.MVC/Controllers/UtstyrDeler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Igland.MVC.Controllers
+{
+    public class UtstyrDeler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
