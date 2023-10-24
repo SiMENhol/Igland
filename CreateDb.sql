ff2de8f4fa2880fe3a3ea10751786bb6f3e94e75
SiMENhol <simenhol@uia.no>
2023-10-24 11:10:57 +0200

    Endret databasen for å ligne UML og ER. Fikset README.md og endret testene.

--- CreateDb.sql
+++ CreateDb.sql
@@ -1,6 +1,6 @@
-drop database webAppDatabase;
-create database if not exists webAppDatabase;
-use webAppDatabase;
+drop database IglandMVCdb;
+create database if not exists IglandMVCdb;
+use IglandMVCdb;
 create table if not EXISTS users
 (
     Id int not null unique auto_increment,
@@ -11,86 +11,82 @@ create table if not EXISTS users
 );
 INSERT INTO users (Id, Name, Email) VALUES ('1', 'Igland Admin', 'Igland@example.com');
 
-create table if not EXISTS AspNetRoles
+ create table if not EXISTS Kunder
 (
-    Id varchar(255) not null,
-    Name varchar(255),
-    NormalizedName  varchar(255),
-    ConcurrencyStamp  varchar(255),
-    CONSTRAINT U_ROLE_ID_PK PRIMARY KEY (Id)
+    KundeId int PRIMARY KEY,
+    KundeNavn varchar(100)
 );
-insert into AspNetRoles(id, Name, NormalizedName) values('Administrator', 'Administrator', 'Administrator');
-create table if not EXISTS AspNetUsers
+ create table if not EXISTS Ordre
 (
-         Id varchar(255) not null unique,
-         UserName varchar(255),
-         NormalizedUserName varchar(255),
-         Email varchar(255),
-         NormalizedEmail varchar(255),
-         EmailConfirmed bit not null,
-         PasswordHash varchar(255),
-         SecurityStamp varchar(255),
-         ConcurrencyStamp varchar(255),
-         PhoneNumber varchar(50),
-         PhoneNumberConfirmed bit not null,
-         TwoFactorEnabled bit not null,
-         LockoutEnd TIMESTAMP,
-         LockoutEnabled bit not null,
-         AccessFailedCount int not null,
-          CONSTRAINT PK_AspNetUsers PRIMARY KEY (Id)
-);
-create table if not EXISTS AspNetUserTokens
-(
-    UserId varchar(255) not null,
-    LoginProvider varchar(255) not null ,
-    Name  varchar(255) not null,
-    Value  varchar(255),
-    CONSTRAINT PK_AspNetUserTokens PRIMARY KEY (UserId, LoginProvider)
-);
-
-create table if not EXISTS AspNetRoleClaims
-(
-    Id int UNIQUE auto_increment,
-    ClaimType varchar(255) not null ,
-    ClaimValue  varchar(255) not null,
-    RoleId  varchar(255),
-    CONSTRAINT PK_AspNetRoleClaims PRIMARY KEY (Id),
-    foreign key(RoleId) 
-        references AspNetRoles(Id)
-);      
-
- create table if not EXISTS AspNetUserClaims
-(
-    Id int UNIQUE auto_increment,
-    ClaimType varchar(255) ,
-    ClaimValue  varchar(255),
-    UserId  varchar(255),
-    CONSTRAINT PK_AspNetRoleClaims PRIMARY KEY (Id),
-    foreign key(UserId) 
-        references AspNetUsers(Id)
-);           
-
- create table if not EXISTS AspNetUserLogins
-(
-    LoginProvider int UNIQUE auto_increment,
-    ProviderKey varchar(255) not null ,
-    ProviderDisplayName  varchar(255) not null,
-    UserId  varchar(255) not null,
-    CONSTRAINT PK_AspNetUserLogins PRIMARY KEY (LoginProvider),
-    foreign key(UserId) 
-        references AspNetUsers(Id)
-);         
-
- create table if not EXISTS AspNetUserRoles
-(
-    UserId varchar(255) not null,
-    RoleId varchar(255) not null,
-    CONSTRAINT PK_AspNetUserRoles PRIMARY KEY (UserId,RoleId),
-    foreign key(UserId) 
-        references AspNetUsers(Id),
-    foreign key(RoleId) 
-        references AspNetRoles(Id)
+    OrdreNummer int PRIMARY KEY,
+    KundeId int,
+    SerieNummer varchar(30),
+    VareNavn varchar(30),
+    Status varchar(30),
+    Arbdokument varchar(255),
+    FOREIGN KEY (KundeId) 
+        REFERENCES Kunder (KundeId)
 ); 
-
-        
-     
+ create table if not EXISTS ServiceSkjema
+(
+    ServiceSkjemaID int PRIMARY KEY,
+    OrdreNummer int,
+    Aarsmodel int,
+    Garanti varchar(255),
+    Reperasjonbeskrivelse varchar(255),
+    MedgåtteDeler varchar(255),
+    DeleRetur varchar(255),
+    ForesendelsesMaate varchar(255),
+    foreign key(OrdreNummer) references Ordre(OrdreNummer)
+);
+ create table if not EXISTS ArbDokument
+(
+    ArbDokumentID int PRIMARY KEY,
+    OrdreNummer int,
+    DatoHenvendelse DATE,
+    AvtaltLevering DATE,
+    ProduktMotttat DATE,
+    AvtaltFerdig DATE,
+    foreign key(OrdreNummer)
+        references Ordre(OrdreNummer)
+);
+ create table if not EXISTS Sjekkliste
+(
+    SjekklisteID int PRIMARY KEY,
+    OrdreNummer int,
+    Sjekklisteinnholdet varchar(255),
+    Kommentar varchar(255),
+    Signatur varchar(100),
+    Dato datetime,
+    SerieNr int,
+    Type varchar(255),
+    foreign key(OrdreNummer)
+        references Ordre(OrdreNummer)
+);
+ create table if not EXISTS SjekklisteItem
+(
+    SjekklisteItemID int PRIMARY KEY,
+    SjekklisteID int,
+    CheckBoksNavn varchar(255),
+    CheckboksValue varchar(255),
+    foreign key(SjekklisteID)
+        references Sjekkliste(SjekklisteID)
+);       
+ create table if not EXISTS KoblingsTabell
+(
+    OrdreNummer int,
+    Avdeling varchar(255),
+    KoblingsTabellID int PRIMARY KEY,
+    TidsbrukPerAvdeling varchar(255),
+    foreign key(OrdreNummer)
+        references Ordre(OrdreNummer)
+);     
+ create table if not EXISTS Avdeling
+(
+    Rolle varchar(255) PRIMARY KEY,
+    KoblingsTabellID int,
+    AvdelingsID int,
+    foreign key(KoblingsTabellID)
+        references KoblingsTabell(KoblingsTabellID)
+);
+SHOW TABLES;
