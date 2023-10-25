ab7f7d73f4b342756de796d20bf03fcc8dd25371
Anastasia Katanova <anastasiak@uia.no>
2023-10-25 15:59:22 +0200

    lagde en model til serviceskjema og endret på html form i servicedoc

--- Igland.MVC/Controllers/NyServiceDocController.cs
+++ Igland.MVC/Controllers/NyServiceDocController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace Igland.MVC.Controllers
 {
     public class NyServiceDocController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
