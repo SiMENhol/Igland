9436b495a6e4ff5167c3cbb6bfa402b1a48a7004
SiMENhol <simenhol@uia.no>
2023-10-25 12:19:52 +0200

    Endre build, til å bygge databsen og kjøre sql script

--- build.cmd
+++ build.cmd
@@ -1,6 +1,18 @@
 @echo off
 
 :: Kill running instance of container
+
+docker kill mariadb
+
+: : Starts the database
+docker run --rm --env "TZ=Europe/Oslo" --name mariadb -p 3308:3306/tcp -v "%cd%\database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=12345 -d mariadb:10.5.11
+
+: : Wait for container to be up
+timeout /t 5
+: : Run sql script
+docker exec -i mariadb mysql  -uroot -p12345 < CreateDb.sql
+
+:: Kill running instance of container
 docker kill webapp
 
 :: Builds image specified in Dockerfile
