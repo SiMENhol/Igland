d1869279d34a6d2dd3b460969ae9520a056ab3e3
Tobias Molland <112409957+ToBeAss@users.noreply.github.com>
2023-11-08 16:22:40 +0100

    Update IServiceSkjema.cs

--- Igland.MVC/Repositories/IRepo/IServiceSkjema.cs
+++ Igland.MVC/Repositories/IRepo/IServiceSkjema.cs
@@ -4,12 +4,8 @@ namespace Igland.MVC.Repositories.IRepo
 {
     public interface IServiceSkjema
     {
-        //  void Upsert(UserEntity user);
-        ServiceDocs Get(int id);
-        List<ServiceDocs> GetAll();
-        
-        // void Update(UserEntity user, List<string> roles);
-        // void Add(UserEntity user);
-        // List<UserEntity> GetUsers();
+        //  void Upsert(ServiceDokumentEntity user);
+        ServiceDokumentEntity Get(int id);
+        List<ServiceDokumentEntity> GetAll();
     }
 }
