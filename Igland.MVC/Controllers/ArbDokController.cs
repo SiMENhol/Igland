6f31a3d168cd5692102ab0e53d91b33ac6769e6c
Tobias Molland <tobiasmol@uia.no>
2023-11-24 15:44:16 +0100

    Har kommentert ArdBokController og lagt til XXS/CSRF

--- Igland.MVC/Controllers/ArbDokController.cs
+++ Igland.MVC/Controllers/ArbDokController.cs
@@ -23,33 +23,59 @@ namespace Igland.MVC.Controllers
             _kunderRepository = kunderRepository;
         }
 
+        /// <summary>
+        /// Get the view of ArbDok/Index, based on the ArbDokFullViewModel, including database data from the arbdok, ordre and kunder repositories.
+        /// </summary>
+        /// <returns>A IActionResult View called "Index" with a model called "model".</returns>
+        [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("Index method called");
             var model = new ArbDokFullViewModel();
             model.ArbDokList = _arbdokRepository.GetAll().Select(x => new ArbDokViewModel { ArbDokID = x.ArbDokID, OrdreNummer = x.OrdreNummer, Kunde = x.Kunde, Vinsj = x.Vinsj, HenvendelseMotatt = x.HenvendelseMotatt, AvtaltLevering = x.AvtaltLevering, ProduktMotatt = x.ProduktMotatt, SjekkUtfort = x.SjekkUtfort, AvtaltFerdig = x.AvtaltFerdig, ServiceFerdig = x.ServiceFerdig, AntallTimer = x.AntallTimer, BestillingFraKunde = x.BestillingFraKunde, NotatFraMekaniker = x.NotatFraMekaniker, Status = x.Status }).ToList();
             model.OrdreList = _ordreRepository.GetAll().Select(x => new OrdreViewModel { OrdreNummer = x.OrdreNummer, KundeID = x.KundeID, SerieNummer = x.SerieNummer, VareNavn = x.VareNavn, Status = x.Status}).ToList();
             model.KunderList = _kunderRepository.GetAll().Select(x => new KunderViewModel { KundeID = x.KundeID, KundeNavn = x.KundeNavn }).ToList();
             return View("Index", model);
         }
+
+        /// <summary>
+        /// Get the view of ArbDok/Ny.
+        /// </summary>
+        /// <returns>A IActionResult View called "Ny".</returns>
         [HttpGet]
+        [ValidateAntiForgeryToken]
         public IActionResult Ny()
         {
-            _logger.LogInformation("Index method called");
-
-            var model = new ArbDokFullViewModel();
-            model.ArbDokList = _arbdokRepository.GetAll().Select(x => new ArbDokViewModel { ArbDokID = x.ArbDokID, OrdreNummer = x.OrdreNummer, Kunde = x.Kunde, Vinsj = x.Vinsj, HenvendelseMotatt = x.HenvendelseMotatt, AvtaltLevering = x.AvtaltLevering, ProduktMotatt = x.ProduktMotatt, SjekkUtfort = x.SjekkUtfort, AvtaltFerdig = x.AvtaltFerdig, ServiceFerdig = x.ServiceFerdig, AntallTimer = x.AntallTimer, BestillingFraKunde = x.BestillingFraKunde, NotatFraMekaniker = x.NotatFraMekaniker, Status = x.Status }).ToList();
-
-            return View("Ny", model);
+            _logger.LogInformation("Ny method called");
+            return View("Ny");
         }
+
+        /// <summary>
+        /// Get the view of ArbDok/Rediger, based on the ArbDokFullViewModel, including database data from the arbdok, ordre and kunder repositories.
+        /// </summary>
+        /// <returns>A IActionResult View called "Rediger" with a model called "model".</returns>
+        [HttpGet]
+        [ValidateAntiForgeryToken]
         public IActionResult Rediger()
         {
+            _logger.LogInformation("Rediger method called");
             var model = new ArbDokFullViewModel();
             model.ArbDokList = _arbdokRepository.GetAll().Select(x => new ArbDokViewModel { ArbDokID = x.ArbDokID, OrdreNummer = x.OrdreNummer, Kunde = x.Kunde, Vinsj = x.Vinsj, HenvendelseMotatt = x.HenvendelseMotatt, AvtaltLevering = x.AvtaltLevering, ProduktMotatt = x.ProduktMotatt, SjekkUtfort = x.SjekkUtfort, AvtaltFerdig = x.AvtaltFerdig, ServiceFerdig = x.ServiceFerdig, AntallTimer = x.AntallTimer, BestillingFraKunde = x.BestillingFraKunde, NotatFraMekaniker = x.NotatFraMekaniker, Status = x.Status }).ToList();
+            model.OrdreList = _ordreRepository.GetAll().Select(x => new OrdreViewModel { OrdreNummer = x.OrdreNummer, KundeID = x.KundeID, SerieNummer = x.SerieNummer, VareNavn = x.VareNavn, Status = x.Status }).ToList();
+            model.KunderList = _kunderRepository.GetAll().Select(x => new KunderViewModel { KundeID = x.KundeID, KundeNavn = x.KundeNavn }).ToList();
             return View("Rediger", model);
         }
 
+        /// <summary>
+        /// Post data from where the method is called, based on the ArbDokFullViewModel, creating an ArbDok entity that is Upserted to the arbdok repository.
+        /// </summary>
+        /// <param name="arbdok">The ArbDokFullViewModel.</param>
+        /// <returns>A IActionResult Redirect to the View called "Index".</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Post(ArbDokFullViewModel arbdok)
         {
+            _logger.LogInformation("Post method called");
             var arbdokEntity = new ArbDok
             {
                 ArbDokID = arbdok.UpsertArbDok.ArbDokID,
@@ -69,26 +95,33 @@ namespace Igland.MVC.Controllers
             };
             _arbdokRepository.Upsert(arbdokEntity);
 
-            /*var ordreEntity = new OrdreEntity
-            {
-                OrdreNummer = arbdok.UpsertOrdre.OrdreNummer,
-                KundeID = arbdok.UpsertOrdre.KundeID, // FK
-                SerieNummer = arbdok.UpsertOrdre.SerieNummer,
-                VareNavn = arbdok.UpsertOrdre.VareNavn,
-                Status = arbdok.UpsertOrdre.Status,
-            };
-            _ordreRepository.Upsert(ordreEntity);
+            /// Old attempt at updating all three repositories at the same time
+                /*var ordreEntity = new OrdreEntity
+                {
+                    OrdreNummer = arbdok.UpsertOrdre.OrdreNummer,
+                    KundeID = arbdok.UpsertOrdre.KundeID, // FK
+                    SerieNummer = arbdok.UpsertOrdre.SerieNummer,
+                    VareNavn = arbdok.UpsertOrdre.VareNavn,
+                    Status = arbdok.UpsertOrdre.Status,
+                };
+                _ordreRepository.Upsert(ordreEntity);
 
-            var kunderEntity = new KunderEntity
-            {
-                KundeID = arbdok.UpsertKunder.KundeID,
-                KundeNavn = arbdok.UpsertKunder.KundeNavn,
-            };
-            _kunderRepository.Upsert(kunderEntity);*/
+                var kunderEntity = new KunderEntity
+                {
+                    KundeID = arbdok.UpsertKunder.KundeID,
+                    KundeNavn = arbdok.UpsertKunder.KundeNavn,
+                };
+                _kunderRepository.Upsert(kunderEntity);*/
 
-            return Redirect("index");
+            return Redirect("Index");
         }
 
+        /// <summary>
+        /// Delete data from repository arbdok, based on ArbDokID.
+        /// </summary>
+        /// <param name="ArbDokID">The unique ID for an ArbDok</param>
+        /// <param name="ArbDokID">The unique ID for an ArbDok</param>
+        /// <returns>A IActionResult RedirectToAction  "Index".</returns>
         [Authorize(Roles = "Administrator")]
         [HttpPost]
         [ValidateAntiForgeryToken]
