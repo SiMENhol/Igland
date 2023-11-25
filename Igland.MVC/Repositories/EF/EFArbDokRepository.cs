abe2031ff727670082b115af84a102aa191a59ca
Tobias Molland <tobiasmol@uia.no>
2023-11-25 14:22:25 +0100

    Kommentarer til ArbDokRepository

--- Igland.MVC/Repositories/EF/EFArbDokRepository.cs
+++ Igland.MVC/Repositories/EF/EFArbDokRepository.cs
@@ -12,14 +12,33 @@ namespace Igland.MVC.Repositories.EF
         {
             this.dataContext = dataContext;
         }
+
+        /// <summary>
+        /// Get an instance from the db table ArbDok based on the ArbDokID.
+        /// </summary>
+        /// <param name="ArbDokID">The ID of the desired instance.</param>
+        /// <returns>An entity of ArbDok.</returns>
         public ArbDok Get(int ArbDokID)
         {
             return dataContext.ArbDok.FirstOrDefault(x => x.ArbDokID == ArbDokID);
         }
+
+        /// <summary>
+        /// Get all instances from the db table ArbDok.
+        /// </summary>
+        /// <returns>A list of entities of ArbDok.</returns>
         public List<ArbDok> GetAll()
         {
             return dataContext.ArbDok.ToList();
         }
+
+        /// <summary>
+        /// Update/Insert.
+        /// If the specified ArbDokID does not exist in the db, a new instance will be created.
+        /// If the specified ArbDokID does exist in the db, the instance will be updated with the specified values.
+        /// </summary>
+        /// <param name="arbdok">The entity that is to be Upserted to the db.</param>
+        /// <returns>Nothing.</returns>
         public void Upsert(ArbDok arbdok)
         {
             var existing = Get(arbdok.ArbDokID);
@@ -45,6 +64,12 @@ namespace Igland.MVC.Repositories.EF
             dataContext.Add(arbdok);
             dataContext.SaveChanges();
         }
+
+        /// <summary>
+        /// Delete an instance from the db table ArbDok based on the ArbDokID.
+        /// </summary>
+        /// <param name="ArbDokID">The ID of the desired instance.</param>
+        /// <returns>Nothing.</returns>
         public void Delete(int ArbDokID)
         {
             ArbDok? arbdok = Get(ArbDokID);
